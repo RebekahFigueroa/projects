91013f8188c0815f7b1291104bf22ee69e33b55e
Rebekah Figueroa <figueroarebekah@gmail.com>
2022-05-08 15:03:34 -0500

    learning document object method

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,15 @@
 
-document.querySelector("h1").innerHTML = "Good Bye";
+//playing around w/ Javascript to learn DOM 
+document.querySelector("li a").style.color="red";  
+
+document.querySelector("h1").style.color="red";
+
+document.querySelector("h1").style.fontSize="10rem";
+
+document.querySelector("button").style.backgroundColor="yellow";
+
+// class list practice
+
+document.querySelector("button").classList.add("invisible");
+
+document.querySelector("h1").classList.add("huge");
